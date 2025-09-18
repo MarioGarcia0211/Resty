49057dc0214b6c4b131c65c1113bf92302eb865c
MarioGarcia0211 <jhonmariogarciaballesteros@gmail.com>
2025-09-18 00:47:25 -0500

    se modifico el grid de la lista de los restaurantes para diferentes tipos de pantalla

--- frontend/src/components/restaurant/RestaurantList.vue
+++ frontend/src/components/restaurant/RestaurantList.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="row g-3">
-    <div v-for="r in restaurants" :key="r._id" class="col-md-4">
+    <div
+      v-for="r in restaurants"
+      :key="r._id"
+      class="col-12 col-sm-6 col-md-4 col-lg-3"
+    >
       <RestaurantCard
         :restaurant="r"
         @edit="$emit('edit', r)"
